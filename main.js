8cfed3e65e93adc91e4e2242d3fc6ec72be5445d
Hayoung Jeon <hayoungj0710@gmail.com>
2021-03-22 12:24:59 +0900

    Add func to find out win/fail
    Add function to check rather user has clicked all the targets
    Add function to check if time's over
    Move click event to popup-wrap
    
    Change variable names to easily find out which means which
    Tide up unneeded codes

--- main.js
+++ main.js
@@ -1,17 +1,25 @@
 'use strict';
 
-const game = document.querySelector('.game');
-const frame = document.querySelector('.find-carrot');
+const gameArea = document.querySelector('.game');
+const playableArea = document.querySelector('.find-carrot');
 const playButton = document.querySelector('.menu__button');
+const counter = document.querySelector('.menu__counter');
+const timer = document.querySelector('.menu__timer');
 const target = document.createElement('img');
 const obstacle = document.createElement('img');
-const diff = document.querySelector('.pop-up__select-diff');
+const popUpWrap = document.querySelector('.pop-up-wrap');
+const diffPopUp = document.querySelector('.pop-up__select-diff');
 const restart = document.querySelector('.pop-up__restart');
+let checkTimer;
+let remainingTarget = 0;
+let remainingTime = 10;
 
 target.setAttribute('src', 'img/carrot.png');
 target.setAttribute('alt', 'carrot');
+target.setAttribute('data-type', 'target');
 obstacle.setAttribute('src', 'img/bug.png');
 obstacle.setAttribute('alt', 'bug');
+obstacle.setAttribute('data-type', 'obstacle');
 
 class Image {
   constructor(target, obstacle, height) {
@@ -20,11 +28,6 @@ class Image {
     this.height = height;
   }
 
-  createImages = () => {
-    this.setImage(target, 'target', this.target);
-    this.setImage(obstacle, 'obstacle', this.obstacle);
-  };
-
   setImageCoord = (item) => {
     const xMax = 700 - item.naturalWidth;
     const yMax = this.height - item.naturalHeight;
@@ -32,13 +35,12 @@ class Image {
     this.y = Math.floor(Math.random() * yMax);
   };
 
-  setImage = (item, itemName, number) => {
-    for (let i = 0; i < number; i++) {
-      item.setAttribute('class', itemName);
+  setImage = (item, itemName) => {
+    for (let i = 0; i < this[itemName]; i++) {
       this.setImageCoord(item);
       item.style.left = `${this.x}px`;
       item.style.top = `${this.y}px`;
-      frame.append(item.cloneNode());
+      playableArea.append(item.cloneNode());
     }
   };
 }
@@ -46,60 +48,111 @@ class Image {
 const changePopUpStatus = (type, status) => {
   const popUp = document.querySelector(`.pop-up--${type}`);
 
+  popUpWrap.style.display = status;
   popUp.style.display = status;
 };
 
-const setDifficulty = (event) => {
-  const selected = event.target.dataset.id;
-  const diff = new Image();
+const setDifficulty = (selectedDiff) => {
+  const gameImage = new Image();
 
-  switch (selected) {
-    case 'easy':
-      game.style.height = '500px';
-      (diff.target = 5), (diff.obstacle = 5), (diff.height = 250);
-      changePopUpStatus('difficulty', 'none');
-      break;
-    case 'medium':
-      game.style.height = '600px';
-      (diff.target = 10), (diff.obstacle = 10), (diff.height = 350);
-      changePopUpStatus('difficulty', 'none');
-      break;
-    case 'extreme':
-      game.style.height = '600px';
-      (diff.target = 10), (diff.obstacle = 15), (diff.height = 350);
-      changePopUpStatus('difficulty', 'none');
-      break;
+  if (selectedDiff) {
+    switch (selectedDiff) {
+      case 'easy':
+        gameArea.style.height = '500px';
+        (gameImage.target = 5),
+          (gameImage.obstacle = 5),
+          (gameImage.height = 250);
+        remainingTarget = 5;
+        break;
+      case 'medium':
+        gameArea.style.height = '600px';
+        (gameImage.target = 10),
+          (gameImage.obstacle = 10),
+          (gameImage.height = 350);
+        remainingTarget = 10;
+        break;
+      case 'extreme':
+        gameArea.style.height = '600px';
+        (gameImage.target = 10),
+          (gameImage.obstacle = 15),
+          (gameImage.height = 350);
+        remainingTarget = 10;
+        break;
+    }
+    counter.innerText = remainingTarget;
   }
 
-  return diff;
+  return gameImage;
 };
 
 playButton.addEventListener('click', () => {
-  if (playButton.classList[1] === 'menu__button--start') {
+  if (playButton.dataset.action === 'start') {
     changePopUpStatus('difficulty', 'block');
-  } else if (playButton.classList[1] === 'menu__button--pause') {
+  } else if (playButton.dataset.action === 'pause') {
     changePopUpStatus('retry', 'block');
+    clearInterval(checkTimer);
   }
 });
-diff.addEventListener('click', (event) => {
-  let diff = new Image();
-  diff = setDifficulty(event);
-  diff.createImages();
-  playButton.classList.remove('menu__button--start');
-  playButton.classList.add('menu__button--pause');
+
+diffPopUp.addEventListener('click', (event) => {
+  const selectedDiff = event.target.dataset.id;
+
+  if (selectedDiff) {
+    const gameImage = setDifficulty(event.target.dataset.id);
+    gameImage.setImage(target, 'target');
+    gameImage.setImage(obstacle, 'obstacle');
+    playButton.classList.remove('menu__button--start');
+    playButton.classList.add('menu__button--pause');
+    playButton.dataset.action = 'pause';
+
+    changePopUpStatus('difficulty', 'none');
+
+    checkTimer = window.setInterval(() => {
+      timer.innerText = `00:0${--remainingTime}`;
+
+      if (remainingTime === 0) {
+        changePopUpStatus('fail', 'block');
+        clearInterval(checkTimer);
+      }
+    }, 1000);
+  }
 });
-frame.addEventListener('click', () => {
-  if (event.target.className === 'target') {
+
+playableArea.addEventListener('click', () => {
+  if (event.target.dataset.type === 'target') {
     event.target.remove();
-  } else if (event.target.className === 'obstacle') {
+    counter.innerText = --remainingTarget;
+
+    const remainEl = playableArea.childNodes;
+    for (const el of remainEl) {
+      if (el.dataset.type === 'target') break;
+      else {
+        changePopUpStatus('win', 'block');
+        clearInterval(checkTimer);
+        break;
+      }
+    }
+  } else if (event.target.dataset.type === 'obstacle') {
     changePopUpStatus('fail', 'block');
-  } else if (event.target.className === 'fas fa-times-circle') {
-    const popUp = event.target.closest('.pop-up');
-    popUp.style.display = 'none';
+    clearInterval(checkTimer);
   }
 });
-document.addEventListener('click', () => {
-  if (event.target.closest('.pop-up__restart')) {
+popUpWrap.addEventListener('click', () => {
+  if (event.target.dataset.work === 'restart') {
     location.reload();
+  } else if (event.target.dataset.work === 'close') {
+    const popUpId = event.target.closest(`.pop-up`).dataset.id;
+    changePopUpStatus(popUpId, 'none');
+
+    if (popUpId === 'retry') {
+      checkTimer = window.setInterval(() => {
+        timer.innerText = `00:0${--remainingTime}`;
+
+        if (remainingTime === 0) {
+          changePopUpStatus('fail', 'block');
+          clearInterval(checkTimer);
+        }
+      }, 1000);
+    }
   }
 });

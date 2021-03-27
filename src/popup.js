e642433f9533e32a6716271fc11b63ab78bd86ab
Hayoung Jeon <hayoungj0710@gmail.com>
2021-03-28 00:15:55 +0900

    Delete code to make close button visible in difficulty pop-up
    Delete code to make close button visible in difficulty pop-up, since it is now always visible

--- src/popup.js
+++ src/popup.js
@@ -3,92 +3,91 @@
 import * as sound from './sound.js';
 
 export class ReStartPopUp {
-	constructor() {
-		this.wrap = document.querySelector('.pop-up-wrap--restart');
-		this.popUp = this.wrap.querySelector('.pop-up');
-		this.content = this.wrap.querySelector('.pop-up__content');
-		this.restart = this.wrap.querySelector('.pop-up__restart');
-		this.close = this.wrap.querySelector('.pop-up__close');
+  constructor() {
+    this.wrap = document.querySelector('.pop-up-wrap--restart');
+    this.popUp = this.wrap.querySelector('.pop-up');
+    this.content = this.wrap.querySelector('.pop-up__content');
+    this.restart = this.wrap.querySelector('.pop-up__restart');
+    this.close = this.wrap.querySelector('.pop-up__close');
 
-		this.restart.addEventListener('click', () => {
-			sound.playButton();
-			this.onRestart && this.onRestart();
-			this.hide();
-		});
-	}
+    this.restart.addEventListener('click', () => {
+      sound.playButton();
+      this.onRestart && this.onRestart();
+      this.hide();
+    });
+  }
 
-	setRestartListener(onRestart) {
-		this.onRestart = onRestart;
-	}
+  setRestartListener(onRestart) {
+    this.onRestart = onRestart;
+  }
 
-	setContentWithText(content) {
-		content && (this.content.innerHTML = content);
-	}
+  setContentWithText(content) {
+    content && (this.content.innerHTML = content);
+  }
 
-	open(content) {
-		this.close.style.visibility = 'hidden';
-		this.setContentWithText(content);
-		this.wrap.classList.remove('pop-up-wrap--hide');
-	}
+  open(content) {
+    this.close.style.visibility = 'hidden';
+    this.setContentWithText(content);
+    this.wrap.classList.remove('pop-up-wrap--hide');
+  }
 
-	hide() {
-		this.wrap.classList.add('pop-up-wrap--hide');
-	}
+  hide() {
+    this.wrap.classList.add('pop-up-wrap--hide');
+  }
 }
 
 export class ClosablePopUp extends ReStartPopUp {
-	constructor() {
-		super();
-		this.close.addEventListener('click', () => {
-			this.onClose && this.onClose();
-			this.hide();
-		});
-	}
+  constructor() {
+    super();
+    this.close.addEventListener('click', () => {
+      this.onClose && this.onClose();
+      this.hide();
+    });
+  }
 
-	setCloseListener(onClose) {
-		this.onClose = onClose;
-	}
+  setCloseListener(onClose) {
+    this.onClose = onClose;
+  }
 
-	open(content) {
-		super.open(content);
-		this.close.style.visibility = 'visible';
-	}
+  open(content) {
+    super.open(content);
+    this.close.style.visibility = 'visible';
+  }
 }
 
 export class DifficultyPopUp {
-	constructor() {
-		this.wrap = document.querySelector('.pop-up-wrap--difficulty');
-		this.selectable = this.wrap.querySelector('.pop-up__select-difficulty');
-		this.close = this.wrap.querySelector('.pop-up__close');
+  constructor() {
+    this.wrap = document.querySelector('.pop-up-wrap--difficulty');
+    this.selectable = this.wrap.querySelector('.pop-up__select-difficulty');
+    this.close = this.wrap.querySelector('.pop-up__close');
 
-		this.selectable.addEventListener('click', (event) => {
-			this.select = event.target.dataset.id;
-			if (this.select) {
-				sound.playButton();
-				this.setDifficulty && this.setDifficulty(this.select);
-				this.hide();
-				this.startGame && this.startGame();
-			}
-		});
-		this.close.addEventListener('click', () => {
-			this.hide();
-		});
-	}
+    this.selectable.addEventListener('click', (event) => {
+      this.select = event.target.dataset.id;
+      if (this.select) {
+        sound.playButton();
+        this.setDifficulty && this.setDifficulty(this.select);
+        this.hide();
+        this.startGame && this.startGame();
+      }
+    });
+    this.close.addEventListener('click', () => {
+      this.hide();
+    });
+  }
 
-	setSelectEventListener(setDifficulty) {
-		this.setDifficulty = setDifficulty;
-	}
+  setSelectEventListener(setDifficulty) {
+    this.setDifficulty = setDifficulty;
+  }
 
-	setStartEventListener(startGame) {
-		this.startGame = startGame;
-	}
+  setStartEventListener(startGame) {
+    this.startGame = startGame;
+  }
 
-	open() {
-		this.close.style.visibility = 'visible';
-		this.wrap.classList.remove('pop-up-wrap--hide');
-	}
+  open() {
+    this.wrap.classList.remove('pop-up-wrap--hide');
+  }
 
-	hide() {
-		this.wrap.classList.add('pop-up-wrap--hide');
-	}
+  hide() {
+    this.wrap.classList.add('pop-up-wrap--hide');
+  }
 }
